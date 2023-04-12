a9bc8aa4e04b7b8113b296c4f1015b74c931a05d
Max Roesler <MaxRoesler@Maxs-MacBook-Pro-46.local>
2023-04-12 11:48:25 -0400

    speech class output for phases

---
+++ speech.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import json
+
+data = json.load(open('respira_Danny_2023-04-06.json', 'r'))
+phases = ['baseline', 'expiration1', 'rest1', 'expiration2', 'rest2', 'video', 'rest3', 'recitation', 'rest4']
+speech = ['speech_happy', 'speech_sad', 'speech_disgust', 'speech_surprise']
+output = {}
+for i in phases:
+	rate = data[i]['stress_rating']
+	maximum = data[i]['speech_happy']
+	key = 'None'
+	for j in speech:
+		if data[i][j] > maximum:
+			key = j
+			maximum = data[i][j]
+	if len(maximum) == 0:
+		j = ''
+		maximum = ''
+	output[i] = str(key + ':' + str(maximum))
+
+for i in output:
+	print(i + ': ' + output[i])
