f32b88a9d9b8f9dfe4532d2baf69ca4e499096e3
Matteo Vidali <mvidali@iu.edu>
2023-04-18 10:42:51 -0400

    Added prototype network connection (HALF WORKS)

--- pyChart/netget.py
+++ pyChart/netget.py
@@ -1,20 +1,42 @@
-from pyp2p.net import Net
+import socket
+import threading
+import time
 
 class NetGet:
     def __init__(self, connection_ip: str=None, connection_port: int=44444):
-        if not connection_ip:
-            return -1
+        self.ip = connection_ip if connection_ip else '140.9.9.9'
+        self.port = connection_port
+        self.socket = None
+        self.connect()
 
-        self.alice = Net(passive_bind=connection_ip, passive_port=connection_port, debug=1)
-        self.alice.start()
-        self.alice.bootstrap()
-        self.alice.advertise()
+    def connect(self):
+        def make_sock():
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+            s.connect((self.ip, self.port))
+            self.socket=s
+        
+        sock = threading.Thread(target=make_sock)
+        sock.start()
+        seconds = 0
+
+        while not self.socket and seconds<10 and sock.is_alive():
+            seconds += 1
+            time.sleep(1)
+        
+        if not self.socket:
+            print("NO SOCKET CREATED")
+            raise ConnectionError
 
     def read_data(self):
-        for con in self.alice:
-            for reply in con:
-                print(reply)
-
+        if self.socket:
+            print("GETTING?")
+            return int(self.socket.recv(1))
+        else:
+            print("NO SOCKET - NO DATA")
+            return -1
 
 if __name__ == "__main__":
     ng = NetGet(connection_ip="140.182.152.100")
+    while True:
+        print(ng.read_data())
